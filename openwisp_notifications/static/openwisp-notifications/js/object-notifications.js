a821bf6054fca7550db8d9992d22497784b94d47
hoatran <hoatran>
2021-08-28 16:10:54 +0700

    init

--- openwisp_notifications/static/openwisp-notifications/js/object-notifications.js
+++ openwisp_notifications/static/openwisp-notifications/js/object-notifications.js
@@ -14,23 +14,24 @@
 })(django.jQuery);
 
 function getObjectNotificationComponent() {
-    return `
-    <li class="ow-object-notification-container">
-        <a id="ow-object-notify" class="ow-object-notify" title="You are receiving notifications for this object.">
-            <span class="ow-icon ow-object-notify-bell"></span>
-            <span id="ow-unsubscribe-label">Unsubscribe</span>
-        </a>
-        <div class="ow-object-notification-option-container option-container ow-hide">
-            <p id="ow-notification-help-text">Disable notifications for</p>
-            <button data-days=0 class="ow-hide ow-notification-option" id="ow-enable-notification">Enable Notifications</button>
-            <button data-days=1 class="ow-notification-option option disable-notification">1 Day</button>
-            <button data-days=7 class="ow-notification-option option disable-notification">1 Week</button>
-            <button data-days=30 class="ow-notification-option option disable-notification">1 Month</button>
-            <button data-days=-1 class="ow-notification-option option disable-notification">Permanently</button>
-            <div id="ow-object-notification-loader" class="ow-hide"><div class="loader"></div></div>
-        </div>
-    </li>
-    `;
+    return ''
+    // return `
+    // <li class="ow-object-notification-container">
+    //     <a id="ow-object-notify" class="ow-object-notify" title="You are receiving notifications for this object.">
+    //         <span class="ow-icon ow-object-notify-bell"></span>
+    //         <span id="ow-unsubscribe-label">Unsubscribe</span>
+    //     </a>
+    //     <div class="ow-object-notification-option-container option-container ow-hide">
+    //         <p id="ow-notification-help-text">Disable notifications for</p>
+    //         <button data-days=0 class="ow-hide ow-notification-option" id="ow-enable-notification">Enable Notifications</button>
+    //         <button data-days=1 class="ow-notification-option option disable-notification">1 Day</button>
+    //         <button data-days=7 class="ow-notification-option option disable-notification">1 Week</button>
+    //         <button data-days=30 class="ow-notification-option option disable-notification">1 Month</button>
+    //         <button data-days=-1 class="ow-notification-option option disable-notification">Permanently</button>
+    //         <div id="ow-object-notification-loader" class="ow-hide"><div class="loader"></div></div>
+    //     </div>
+    // </li>
+    // `;
 }
 
 function initObjectNotificationDropdown($) {
